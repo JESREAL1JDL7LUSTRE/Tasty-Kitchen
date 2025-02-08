df937939ec42857eb5585f80d6fea1f3b7906e29
baeeyuh <bacaling.beaclarise@gmail.com>
2025-02-09 03:05:33 +0800

    backend and frontend files now connected

--- backend/posts/api/urls.py
+++ backend/posts/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import get_posts
+from rest_framework.routers import DefaultRouter
+from.views import PostViewSet
 
-urlpatterns = [
-    path('posts/', get_posts, name='posts'),
-]
+post_router = DefaultRouter()
+post_router.register(r'posts', PostViewSet)
